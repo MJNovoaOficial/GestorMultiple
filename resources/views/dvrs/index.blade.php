ec9d242e4492f4f56b3a888014ac15177390b2cf
Maria Jose Novoa <nmariajose7@gmail.com>
2026-08-07 12:18:14 -0400

    diseño visual

--- resources/views/dvrs/index.blade.php
+++ resources/views/dvrs/index.blade.php
@@ -19,7 +19,13 @@
                 <button
                     type="button"
                     id="open-export-modal"
-                    class="px-4 py-2 bg-green-600 hover:bg-green-700 text-white rounded-lg">
+                    class="px-5 py-3
+                        rounded-2xl
+                        bg-green-600
+                        hover:bg-green-700
+                        transition
+                        text-white
+                        font-semibold">
                     Exportar
                 </button>
 
